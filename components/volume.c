4407af18c3b96ef2677c57720f11a747b0177c62
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-10-28 10:31:37 -0400

    Added Alsa volume status

--- components/volume.c
+++ components/volume.c
@@ -1,45 +1,58 @@
 /* See LICENSE file for copyright and license details. */
+#include <alsa/asoundlib.h>
+#include <alsa/mixer.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
-#if defined(__OpenBSD__)
-	#include <soundcard.h>
-#else
-	#include <sys/soundcard.h>
-#endif
 #include <sys/ioctl.h>
+#include <sys/soundcard.h>
 #include <unistd.h>
-
 #include "../util.h"
 
-const char *
-vol_perc(const char *card)
-{
-	size_t i;
-	int v, afd, devmask;
-	char *vnames[] = SOUND_DEVICE_NAMES;
+const char* vol_perc(const char* card) {
+  snd_mixer_t* handle;
+  snd_mixer_elem_t* elem;
+  snd_mixer_selem_id_t* sid;
 
-	if ((afd = open(card, O_RDONLY | O_NONBLOCK)) < 0) {
-		warn("open '%s':", card);
-		return NULL;
-	}
+  static const char* mix_name = "Master";
+  //static const char* card = "default";
+  static int mix_index = 0;
 
-	if (ioctl(afd, (int)SOUND_MIXER_READ_DEVMASK, &devmask) < 0) {
-		warn("ioctl 'SOUND_MIXER_READ_DEVMASK':");
-		close(afd);
-		return NULL;
-	}
-	for (i = 0; i < LEN(vnames); i++) {
-		if (devmask & (1 << i) && !strcmp("vol", vnames[i])) {
-			if (ioctl(afd, MIXER_READ(i), &v) < 0) {
-				warn("ioctl 'MIXER_READ(%ld)':", i);
-				close(afd);
-				return NULL;
-			}
-		}
-	}
+  int ret = 0;
+  long minv, maxv, out_vol;
 
-	close(afd);
+  snd_mixer_selem_id_alloca(&sid);
 
-	return bprintf("%d", v & 0xff);
+  // sets simple-mixer index and name
+  snd_mixer_selem_id_set_index(sid, mix_index);
+  snd_mixer_selem_id_set_name(sid, mix_name);
+
+  if ((snd_mixer_open(&handle, 0)) < 0) return NULL;
+  if ((snd_mixer_attach(handle, card)) < 0) {
+    snd_mixer_close(handle);
+    return NULL;
+  }
+  if ((snd_mixer_selem_register(handle, NULL, NULL)) < 0) {
+    snd_mixer_close(handle);
+    return NULL;
+  }
+  ret = snd_mixer_load(handle);
+  if (ret < 0) {
+    snd_mixer_close(handle);
+    return NULL;
+  }
+  elem = snd_mixer_find_selem(handle, sid);
+  if (!elem) {
+    snd_mixer_close(handle);
+    return NULL;
+  }
+
+  snd_mixer_selem_get_playback_volume_range(elem, &minv, &maxv); 
+  if (snd_mixer_selem_get_playback_volume(elem, 0, &out_vol) < 0) {
+    snd_mixer_close(handle);
+    return NULL;
+  }
+
+  snd_mixer_close(handle);
+  return bprintf("%d", out_vol & 0xff);
 }
