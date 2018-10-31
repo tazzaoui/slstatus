e10e9d481dc68d9dea2c9f570db33f2163368f5b
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-10-30 23:28:51 -0400

    Added mute check

--- components/volume.c
+++ components/volume.c
@@ -9,13 +9,32 @@
 #include <unistd.h>
 #include "../util.h"
 
+int is_muted(void) {
+  char cmd[512], buffer[16];
+  FILE* pipe;
+
+  // TODO: Find a way to check for mute in C without running a shell command
+  strcpy(cmd, "pacmd list-sinks | awk '/muted/ {print $2}'");
+  pipe = popen(cmd, "r");
+
+  if (!pipe) return -1;
+
+  fgets(buffer, sizeof(buffer), pipe);
+  buffer[strlen(buffer) - 1] = 0x0;
+  pclose(pipe);
+
+  return !strcmp(buffer, "yes");
+}
+
 const char* vol_perc(const char* card) {
   snd_mixer_t* handle;
   snd_mixer_elem_t* elem;
   snd_mixer_selem_id_t* sid;
 
+  if (is_muted()) return bprintf("%d", 0);
+
   static const char* mix_name = "Master";
-  //static const char* card = "default";
+  // static const char* card = "default";
   static int mix_index = 0;
 
   int ret = 0;
@@ -47,7 +66,7 @@ const char* vol_perc(const char* card) {
     return NULL;
   }
 
-  snd_mixer_selem_get_playback_volume_range(elem, &minv, &maxv); 
+  snd_mixer_selem_get_playback_volume_range(elem, &minv, &maxv);
   if (snd_mixer_selem_get_playback_volume(elem, 0, &out_vol) < 0) {
     snd_mixer_close(handle);
     return NULL;
